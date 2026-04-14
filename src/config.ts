0ea0c8c88ac3709b60e8ad04102ed86c13541ab5
jian-guo19 <95682050+jian-guo19@users.noreply.github.com>
2026-04-14 15:13:16 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -369,7 +369,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-	name: "松坂由纪",
+	name: "坚果杜",
 	bio: "世界很大，你必须去看看",
 	typewriter: {
 		enable: true, // 启用个人简介打字机效果
