3820927c25101a21295c2274276228a684bf5a10
jian-guo19 <95682050+jian-guo19@users.noreply.github.com>
2026-04-14 15:20:52 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -368,7 +368,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/Watt-nut.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	avatar: "assets/images/Wall-nut.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "坚果杜",
 	bio: "世界很大，你必须去看看",
 	typewriter: {
